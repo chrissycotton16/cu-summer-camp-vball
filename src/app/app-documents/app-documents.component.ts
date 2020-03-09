0fda12647d62a68ccc476831978184b4f28acd95
chrissycotton16 <c.n.cotton@eagle.clarion.edu>
2020-03-08 20:27:24 -0400

    Update navbar

---
+++ src/app/app-documents/app-documents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-documents',
+  templateUrl: './app-documents.component.html',
+  styleUrls: ['./app-documents.component.css']
+})
+export class AppDocumentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Ng2CarouselamosModule} from 'ng2-carouselamos';
 import { AppHeaderComponent } from './app-header/app-header.component';
-import { AppSlideshowComponent } from './app-slideshow/app-slideshow.component'
+import { AppSlideshowComponent } from './app-slideshow/app-slideshow.component';
+import { AppDocumentsComponent } from './app-documents/app-documents.component';
+import {PdfViewerModule} from 'ng2-pdf-viewer';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
-    AppSlideshowComponent
+    AppSlideshowComponent,
+    AppDocumentsComponent
   ],
   imports: [
     BrowserModule,
-    Ng2CarouselamosModule
+    Ng2CarouselamosModule,
+    PdfViewerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
