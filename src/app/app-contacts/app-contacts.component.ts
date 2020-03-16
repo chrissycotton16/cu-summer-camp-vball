8c768310c56b66e6ae805d72a269e14d88d7a6fb
chrissycotton16 <c.n.cotton@eagle.clarion.edu>
2020-03-16 09:58:36 -0400

    Temp changes for Mac

--- src/app/app-contacts/app-contacts.component.ts
+++ src/app/app-contacts/app-contacts.component.ts
@@ -7,7 +7,9 @@ export interface dumbyData {
   phone: string;
 } 
 
-const ELEMENT_DATA: dumbyData[] =[];
+const ELEMENT_DATA: dumbyData[] =[{name:'A',number:1,phone:'1234'}];
+
+  
 @Component({
   selector: 'app-app-contacts',
   templateUrl: './app-contacts.component.html',
@@ -16,15 +18,25 @@ const ELEMENT_DATA: dumbyData[] =[];
 export class AppContactsComponent implements OnInit {
   displayedColumns: string[] = ['name', 'number', 'phone'];
   dataSource = ELEMENT_DATA;
+  data: Blob;
 
-  constructor() { }
+  constructor() {
+    console.log("hello");
+
+   }
 
   ngOnInit() {
+    let data='src/assets/dumbyData.csv';
+    let reader = new FileReader();
+    reader.readAsText(this.data);
+    reader.onload = function(){console.log(reader.result);}
+    reader.onerror =function(){console.log(reader.error);}
+
   }
 
 }
 
 function getData(){
-
+  
 
 }
